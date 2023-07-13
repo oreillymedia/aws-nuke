8daa21232818985a8989eb34c48376d4a5f82913
Sherd White <swhite@oreilly.com>
2023-07-13 11:59:27 -0500

    Adding opensearch vpcendpoints functionality.
    Removing unused var from packages.

--- resources/opensearchservice-vpcendpoints.go
+++ resources/opensearchservice-vpcendpoints.go
@@ -1,72 +1,45 @@
 package resources
 
 import (
-	"context"
-
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/opensearchservice"
-
-	"github.com/ekristen/libnuke/pkg/registry"
-	"github.com/ekristen/libnuke/pkg/resource"
-	"github.com/ekristen/libnuke/pkg/types"
-
-	"github.com/ekristen/aws-nuke/v3/pkg/nuke"
+	"github.com/rebuy-de/aws-nuke/v2/pkg/types"
 )
 
-const OSVPCEndpointResource = "OSVPCEndpoint"
-
-func init() {
-	registry.Register(&registry.Registration{
-		Name:   OSVPCEndpointResource,
-		Scope:  nuke.Account,
-		Lister: &OSVPCEndpointLister{},
-	})
+type OSVPCEndpoint struct {
+	svc           *opensearchservice.OpenSearchService
+	vpcEndpointId *string
 }
 
-type OSVPCEndpointLister struct{}
+func init() {
+	register("OSVPCEndpoint", ListOSVPCEndpoints)
+}
 
-func (l *OSVPCEndpointLister) List(_ context.Context, o interface{}) ([]resource.Resource, error) {
-	opts := o.(*nuke.ListerOpts)
+func ListOSVPCEndpoints(sess *session.Session) ([]Resource, error) {
+	svc := opensearchservice.New(sess)
 
-	svc := opensearchservice.New(opts.Session)
-	resources := make([]resource.Resource, 0)
-	var nextToken *string
+	listResp, err := svc.DescribeVpcEndpoints(&opensearchservice.DescribeVpcEndpointsInput{
+		VpcEndpointIds: []*string{},
+	})
+	if err != nil {
+		return nil, err
+	}
 
-	for {
-		params := &opensearchservice.ListVpcEndpointsInput{
-			NextToken: nextToken,
-		}
-		listResp, err := svc.ListVpcEndpoints(params)
-		if err != nil {
-			return nil, err
-		}
+	resources := make([]Resource, 0)
 
-		for _, vpcEndpoint := range listResp.VpcEndpointSummaryList {
-			resources = append(resources, &OSVPCEndpoint{
-				svc:           svc,
-				vpcEndpointID: vpcEndpoint.VpcEndpointId,
-			})
-		}
-
-		// Check if there are more results
-		if listResp.NextToken == nil {
-			break // No more results, exit the loop
-		}
-
-		// Set the nextToken for the next iteration
-		nextToken = listResp.NextToken
+	for _, vpcEndpoint := range listResp.VpcEndpoints {
+		resources = append(resources, &OSVPCEndpoint{
+			svc:           svc,
+			vpcEndpointId: vpcEndpoint.VpcEndpointId,
+		})
 	}
 
 	return resources, nil
 }
 
-type OSVPCEndpoint struct {
-	svc           *opensearchservice.OpenSearchService
-	vpcEndpointID *string
-}
-
-func (o *OSVPCEndpoint) Remove(_ context.Context) error {
+func (o *OSVPCEndpoint) Remove() error {
 	_, err := o.svc.DeleteVpcEndpoint(&opensearchservice.DeleteVpcEndpointInput{
-		VpcEndpointId: o.vpcEndpointID,
+		VpcEndpointId: o.vpcEndpointId,
 	})
 
 	return err
@@ -74,10 +47,9 @@ func (o *OSVPCEndpoint) Remove(_ context.Context) error {
 
 func (o *OSVPCEndpoint) Properties() types.Properties {
 	properties := types.NewProperties()
-	properties.Set("VpcEndpointId", o.vpcEndpointID)
 	return properties
 }
 
 func (o *OSVPCEndpoint) String() string {
-	return *o.vpcEndpointID
+	return *o.vpcEndpointId
 }
