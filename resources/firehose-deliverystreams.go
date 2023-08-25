cbc61117e8352202c4e0b40609e34359ab85df3a
Mike Schouw <49021968+MikeSchouw@users.noreply.github.com>
2023-08-25 10:36:53 +0200

    Adding tag support for FirehoseDeliveryStreams  (#1088)
    * adding code for removing custom origin request policies
    
    * rename resource
    
    * test for replication
    
    * revert secretsmanager change
    
    * undo this
    
    * revert mod and sum changes
    
    * add resources for redshift scheduled actions
    
    * remove cloudfront resource
    
    * clean up
    
    * cloudwatch rum app
    
    * remove rum
    
    * add cloudfront origin request policy
    
    * remove
    
    * test release pipeline
    
    * add these two
    
    * update agent to self-hosted
    
    * initial commit without storing the latest tag, max tags = 1
    
    * initial commit without storing the latest tag, max tags = 1
    
    * simplified the if-statement
    
    * PR ready
    
    * Update release.yaml

--- resources/firehose-deliverystreams.go
+++ resources/firehose-deliverystreams.go
@@ -7,14 +7,14 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/firehose"
-
-	"github.com/ekristen/libnuke/pkg/registry"
-	"github.com/ekristen/libnuke/pkg/resource"
-
-	"github.com/ekristen/aws-nuke/v3/pkg/nuke"
+	"github.com/rebuy-de/aws-nuke/v2/pkg/types"
 )
 
-const FirehoseDeliveryStreamResource = "FirehoseDeliveryStream"
+type FirehoseDeliveryStream struct {
+	svc                *firehose.Firehose
+	deliveryStreamName *string
+	tags               []*firehose.Tag
+}
 
 func init() {
 	registry.Register(&registry.Registration{
@@ -24,13 +24,10 @@ func init() {
 	})
 }
 
-type FirehoseDeliveryStreamLister struct{}
-
-func (l *FirehoseDeliveryStreamLister) List(_ context.Context, o interface{}) ([]resource.Resource, error) {
-	opts := o.(*nuke.ListerOpts)
-
-	svc := firehose.New(opts.Session)
-	resources := make([]resource.Resource, 0)
+func ListFirehoseDeliveryStreams(sess *session.Session) ([]Resource, error) {
+	svc := firehose.New(sess)
+	resources := []Resource{}
+	tags := []*firehose.Tag{}
 	var lastDeliveryStreamName *string
 
 	params := &firehose.ListDeliveryStreamsInput{
@@ -44,14 +41,35 @@ func (l *FirehoseDeliveryStreamLister) List(_ context.Context, o interface{}) ([
 		}
 
 		for _, deliveryStreamName := range output.DeliveryStreamNames {
+			tagParams := &firehose.ListTagsForDeliveryStreamInput{
+				DeliveryStreamName: deliveryStreamName,
+				Limit:              aws.Int64(50),
+			}
+
+			for {
+				tagResp, tagErr := svc.ListTagsForDeliveryStream(tagParams)
+				if tagErr != nil {
+					return nil, tagErr
+				}
+
+				tags = append(tags, tagResp.Tags...)
+				if !*tagResp.HasMoreTags {
+					break
+				}
+
+				tagParams.ExclusiveStartTagKey = tagResp.Tags[len(tagResp.Tags)-1].Key
+			}
+
 			resources = append(resources, &FirehoseDeliveryStream{
 				svc:                svc,
 				deliveryStreamName: deliveryStreamName,
+				tags:               tags,
 			})
+
 			lastDeliveryStreamName = deliveryStreamName
 		}
 
-		if !ptr.ToBool(output.HasMoreDeliveryStreams) {
+		if !*output.HasMoreDeliveryStreams {
 			break
 		}
 
@@ -76,4 +94,14 @@ func (f *FirehoseDeliveryStream) Remove(_ context.Context) error {
 
 func (f *FirehoseDeliveryStream) String() string {
 	return *f.deliveryStreamName
+}
+
+func (f *FirehoseDeliveryStream) Properties() types.Properties {
+	properties := types.NewProperties()
+	for _, tag := range f.tags {
+		properties.SetTag(tag.Key, tag.Value)
+	}
+
+	properties.Set("Name", f.deliveryStreamName)
+	return properties
 }
